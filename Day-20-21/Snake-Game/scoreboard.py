88fdc337b77934d2ebd410c7a1c23661f23a56e9
Marlowe-Haws <marlowe.a.h@gmail.com>
2025-06-11 11:48:36 -0600

    Create scoreboard.py

---
+++ Day-20-21/Snake-Game/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0, 260)
+
+    def display_score(self):
+        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def update_score(self):
+        self.score += 1
+        self.clear()
+        self.display_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
