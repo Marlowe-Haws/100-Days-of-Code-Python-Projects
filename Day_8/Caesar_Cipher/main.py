bc231fe0f90d6627c7993ca417a77890ccf91c98
Marlowe-Haws <marlowe.a.h@gmail.com>
2025-06-25 10:00:25 -0600

    Update main.py

--- Day_8/Caesar_Cipher/main.py
+++ Day_8/Caesar_Cipher/main.py
@@ -1,16 +1,18 @@
 from art import logo
 print(logo)
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
     if encode_or_decode == "decode":
         shift_amount *= -1
-    for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+    for char in original_text:
+        if char in alphabet:
+            shifted_position = alphabet.index(char) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+        	output_text += char
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 while True:
@@ -24,14 +26,8 @@ while True:
             continue
         else:
             break
-
-    while True:
-        text = input("Type your message: ").lower()
-        if not text.isalpha():
-            print("Please enter letters only.")
-            continue
-        else:
-            break
+            
+    text = input("Type your message: ").lower()
 
     while True:
         shift = input("Type the shift number: ")
